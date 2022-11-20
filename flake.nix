bc36178896c206cdff8978bbc5eca88bd9507e03
Luca Leon Happel <lucahappel99@gmx.de>
2022-11-20 13:38:56 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Monti (remote monitoring system) backend restful API";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        customPython = (pkgs.python310.withPackages(ps : with ps; [
+            flask
+            ipython
+            pandas
+            matplotlib
+          ]));
+      in
+        rec {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              customPython
+              git
+            ];
+          };
+        }
+      );
+}
