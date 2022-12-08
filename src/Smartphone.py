78683272f73a160f7ad06adc7a1c2515e413d19e
Luca Leon Happel <luca.happel@hhu.de>
2022-12-08 23:53:18 +0100

    add smartphone class

--- src/Smartphone.py
+++ src/Smartphone.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 import json
+from typing import Optional
 
 
 @dataclass
@@ -36,3 +37,27 @@ class Smartphone:
         Erstelle ein Smartphone aus einer JSON-Repräsentation
         """
         return Smartphone(**json.loads(json_string))
+
+    @staticmethod
+    def from_request_form(form: dict) -> Optional[Smartphone]:
+        """
+        Erstelle ein Smartphone aus einem Formular
+        """
+        if not "smartphone_brand" in form:
+            raise ValueError("Smartphone brand not found in form")
+        if not "smartphone_model" in form:
+            raise ValueError("Smartphone model not found in form")
+        if not "smartphone_operating_system" in form:
+            raise ValueError("Smartphone operating system not found in form")
+        if not "smartphone_monti_version" in form:
+            raise ValueError("Smartphone monti version not found in form")
+        if not "smartphone_id" in form:
+            raise ValueError("Smartphone ID not found in form")
+
+        return Smartphone(
+            brand=form['smartphone_brand'],
+            model=form['smartphone_model'],
+            operating_system=form['smartphone_operating_system'],
+            monti_version=form['smartphone_monti_version'],
+            id=form['smartphone_id']
+        )
