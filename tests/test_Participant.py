2e4b1f514b29d8df818835ccfa3c5f2d6f7cc62e
Luca Leon Happel <luca.happel@hhu.de>
2022-12-08 14:51:17 +0100

    participant creation unittest finished

--- tests/test_Participant.py
+++ tests/test_Participant.py
@@ -1,3 +1,4 @@
+import io
 import os
 
 from _pytest import monkeypatch
@@ -8,14 +9,19 @@ def test_participant_init():
     participant = src.Participant.Participant()
     assert participant is not None
 
-def test_create_new_participant_creates_directory(mocker):
+def test_create_new_participant(mocker, tmp_path):
     # Set up the mock
     mocker.patch.object(os, 'mkdir', return_value=None)
+    mock_file = io.StringIO()
+    mocker.patch.object(mock_file, 'close', return_value=None)
+    mocker.patch('builtins.open', return_value=mock_file)
 
     # Call the function being tested
-    study_participant_path = "./data/testStudie/participants"
     participant = src.Participant.Participant()
-    participant.create(study_participant_path)
+    participant.create(tmp_path)
 
     # Verify that os.mkdir was called with the correct arguments
-    os.mkdir.assert_called_with(f"{study_participant_path}/{participant.id}")
+    os.mkdir.assert_called_with(f"{tmp_path}/{participant.id}")
+    # TODO: Verify that the file was written to
+    mock_file.seek(0)
+    assert mock_file.read() == participant.to_json()
